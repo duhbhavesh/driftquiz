683003cb04fb12894249034c9c9b42499f1495c8
Bhavesh Kasturi <connect.bhavesh.kasturi@gmail.com>
2021-05-17 12:40:39 +0530

    feat: setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Driftquiz</h1>
+    <div>
+      <h1 className="text-center text-2xl font-bold">Driftquiz</h1>
     </div>
   );
 }
